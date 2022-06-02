a256bd15f136e53a23262077c69329bd7d81b817
Muhammad Alwi Ramadhan <alwi.romadhon@gmail.com>
2022-06-02 08:25:43 +0700

    coba push

---
+++ Assets/Script/Script Game/Ninja2.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ninja2 : MonoBehaviour
+{
+    public GameObject stand;
+    public GameObject crouch;
+    public float slideSpeed = 5;
+
+    Rigidbody2D rb;
+
+    public GameManager gameManager;
+
+    AudioSource slideSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slideSound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp("down"))
+        {
+            stand.SetActive(true);
+            crouch.SetActive(false);
+
+            slideSound.Play();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "obstacle")
+        {
+            gameManager.GameOver();
+        }
+    }
+
+    public void slideButton() {
+        
+            stand.SetActive(false);
+            crouch.SetActive(true);
+            slideSound.Play();
+            stand.SetActive(true);
+            crouch.SetActive(false);
+        
+    }
+}
---
+++ Assets/Script/Script Game/Obstacle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    float maxTime;
+    float timer;
+
+    public GameObject obstacle1;
+    public GameObject obstacle2;
+
+    int chooseObstacle;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxTime = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer >= maxTime)
+        {
+            GenerateObstacle();
+            timer = 0;
+        }
+    }
+
+    void GenerateObstacle()
+    {
+        chooseObstacle = Random.Range(1, 3);
+        if (chooseObstacle == 1) { GameObject newObstacle = Instantiate(obstacle1); }
+        if (chooseObstacle == 2) { GameObject newObstacle = Instantiate(obstacle2); }
+    }
+}
